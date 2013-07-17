368d5e078dc2f9a05a51179d7d53f1ea191b6a0e
lucb1e <git-luc-laptop2@lucb1e.com>
2013-07-17 17:25:09 +0200

    Multiple dirs allowed as argument; set executable bit

--- randomdir.php
+++ randomdir.php
@@ -1,55 +1,55 @@
 #!/usr/bin/env php
 <?php
-$verbose = false;
-$dir = ".";
-foreach ($argv as $num=>$val) {
-        if ($num == 0)
-                continue;
-
-        if ($val[0] == "-")
-                switch ($val) {
-                        case "-v":
-                                $verbose = true;
-                                break;
-                }
-        else
-                $dir = $val;
-}
-
-if ($dir[strlen($dir) - 1] != '/')
-        $dir .= '/';
-
 $chosen_dir = '';
 $dirs = 0;
 
-function parsedir($dir) {
-        global $chosen_dir, $dirs, $verbose;
+$dir = ".";
+$verbose = false;
+foreach ($argv as $num=>$val) {
+	if ($num == 0)
+		continue;
 
-        $d = opendir($dir);
-        if ($d) {
-                if ($verbose)
-                        echo "Parsing $dir\n";
-
-                while ($f = readdir($d)) {
-                        if ($f == '.' || $f == '..')
-                                continue;
-
-                        if (is_dir($dir . $f) && is_link($dir . $f) === false)
-                                parsedir($dir . $f . '/');
-
-                }
-
-                if (rand(0, $dirs) == 0)
-                        $chosen_dir = $dir;
-
-                $dirs++;
-                closedir($d);
-        }
-        else {
-                if ($verbose)
-                        echo "Skipping $dir (error opening)\n";
-        }
+	if ($val[0] == "-")
+		switch ($val) {
+			case "-v":
+				$verbose = true;
+				break;
+		}
+	else
+		parsedir($val);
 }
 
-parsedir($dir);
-echo 'cd ' . $chosen_dir . "\n";
+
+
+function parsedir($dir) {
+	global $chosen_dir, $dirs, $verbose;
+
+	if ($dir[strlen($dir) - 1] != '/')
+		$dir .= '/';
+	$d = opendir($dir);
+	if ($d) {
+		if ($verbose)
+			echo "Parsing $dir\n";
+
+		while ($f = readdir($d)) {
+			if ($f == '.' || $f == '..')
+				continue;
+
+			if (is_dir($dir . $f) && is_link($dir . $f) === false)
+				parsedir($dir . $f . '/');
+
+		}
+
+		if (rand(0, $dirs) == 0)
+			$chosen_dir = $dir;
+
+		$dirs++;
+		closedir($d);
+	}
+	else {
+		if ($verbose)
+			echo "Skipping $dir (error opening)\n";
+	}
+}
+
+echo $chosen_dir . "\n";
